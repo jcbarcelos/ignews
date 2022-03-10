8a05603be8779dd788c2c53c0ba42ec15e1272a0
jcbarcelos <jcbarcelosrj@gmail.com>
2022-03-10 13:53:49 -0300

    subscribe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,16 +1,46 @@
+import { query as q } from "faunadb";
 import { stripe } from "../../services/stripe";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/faunadb";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
-    const stripeCustomers = await  stripe.customers.create({
-        email: session.user.email
+
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    let customerId = user.data.stripe_customer_id;
+    if (!customerId) {
+      const stripeCustomers = await stripe.customers.create({
+        email: session.user.email,
+        name: session.user.name,
         //metadata
-    })
+      });
+    
+
+    await fauna.query(
+      q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+        data: {
+          stripe_customer_id: stripeCustomers.id,
+        },
+      })
+    );
+    customerId = stripeCustomers.id
+  }
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomers.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1KTsehKWiRn3lmJmqtr9mioy", quantity: 1 }],
@@ -19,7 +49,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       success_url: process.env.STRIPE_SUCCESS_URL,
       cancel_url: process.env.STRIPE_CANCEL_URL,
     });
-    return res.status(200).json({sessionId: stripeCheckoutSession.id})
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
